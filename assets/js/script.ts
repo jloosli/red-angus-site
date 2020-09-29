bbd280811b90410dc1847e2e48e189056f013309
Jared Loosli <jared-loosli@pluralsight.com>
2020-09-29 08:17:40 -0600

    Hack to get image resize working again

--- assets/js/script.ts
+++ assets/js/script.ts
@@ -1,8 +1,9 @@
 // core version + navigation, pagination modules:
-import Swiper, { Navigation,  Autoplay, Lazy } from 'swiper';
+import Swiper, {Navigation, Autoplay, Lazy} from 'swiper';
 
 // configure Swiper to use modules
 Swiper.use([Navigation, Autoplay, Lazy]);
+
 
 const mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
@@ -21,14 +22,20 @@ const mySwiper = new Swiper('.swiper-container', {
     el: '.swiper-scrollbar',
   },
   autoplay: true,
-})
+});
 
-const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector(".nav-header");
+// Hack to get the image to resize correctly
+window.document.addEventListener('DOMContentLoaded', (event) => {
+  mySwiper.update();
+});
+
+
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav-header');
 // On click
-hamburger.addEventListener("click", function() {
+hamburger.addEventListener('click', function () {
   // Toggle class "is-active"
-  hamburger.classList.toggle("is-active");
+  hamburger.classList.toggle('is-active');
   nav.classList.toggle('is-active');
   // Do something else, like open/close menu
 });
