c08a6d488e365db71f269777832c8af5599f3dc1
Jared Loosli <jared-loosli@pluralsight.com>
2020-09-29 11:10:26 -0600

    More swiper cleanup

--- assets/js/_swiper.ts
+++ assets/js/_swiper.ts
@@ -8,23 +8,15 @@ Swiper.use([Navigation, Autoplay, Lazy]);
 const mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
   loop: true,
-  lazy: true,
-  preloadImages: true,
+  lazy: {
+    loadPrevNext: true,
+  },
+  preloadImages: false,
 
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
   autoplay: true,
-});
-
-// Hack to get the image to resize correctly
-window.document.addEventListener('DOMContentLoaded', (event) => {
-  mySwiper.update();
 });
