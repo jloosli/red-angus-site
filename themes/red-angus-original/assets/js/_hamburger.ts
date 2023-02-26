bf913aeb13c6fe3c7772d3503c48dbf36416b8a1
Jared Loosli <jloosli@gmail.com>
2023-02-25 19:12:04 -0700

    Removed old Template, fixed issue with vimeo ID getting mislabeled after minified

--- themes/red-angus-original/assets/js/_hamburger.ts
+++
@@ -1,9 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav-header');
-// On click
-hamburger.addEventListener('click', function () {
-  // Toggle class "is-active"
-  hamburger.classList.toggle('is-active');
-  nav.classList.toggle('is-active');
-  // Do something else, like open/close menu
-});
