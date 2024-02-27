14c3f01358fd062efd867dc8d697bd5028ebfe70
Jared Loosli <jloosli@gmail.com>
2024-02-27 21:41:51 +0530

    Formatted file

--- utilities/set_image_resource_metadata.py
+++ utilities/set_image_resource_metadata.py
@@ -8,10 +8,12 @@ from os import listdir
 from os.path import isfile, join, dirname
 import yaml
 
+YEAR = "2023"
 FILE_PATH = dirname(__file__)
-BULLS_PATH = join(FILE_PATH, '..','content','sales','2023','bulls')
-IMAGES_PATH = join(BULLS_PATH , 'images')
-EPD_FILE = '2023 Bull EPD\'s Feb 12.xlsx - Sheet1.csv'
+BULLS_PATH = join(FILE_PATH, "..", "content", "sales", YEAR, "bulls")
+IMAGES_PATH = join(BULLS_PATH, "images")
+EPD_FILE = "2023 Bull EPD's Feb 12.xlsx - Sheet1.csv"
+
 
 def find_in_list(l, target):
     for i in l:
@@ -21,8 +23,11 @@ def find_in_list(l, target):
 
 
 pattern = re.compile("\\.md|.DS_Store$")
-onlyfiles = [{'src': f, 'params': {'tag': f.split('_')[0], 'lot': 0}} for f in listdir(IMAGES_PATH) if
-             isfile(join(IMAGES_PATH, f)) and not pattern.search(f)]
+onlyfiles = [
+    {"src": f, "params": {"tag": f.split("_")[0], "lot": 0}}
+    for f in listdir(IMAGES_PATH)
+    if isfile(join(IMAGES_PATH, f)) and not pattern.search(f)
+]
 
 resources = []
 
@@ -34,13 +39,15 @@ for f in onlyfiles:
     tempFile = f.copy()
 
     for row in data:
-        if tempFile['params']['tag'] in row['Name']:
-            tempFile['params']['lot'] = int(row['Lot #'])
-            tempFile['name'] = row['Name']
-            tempFile['title'] = "Lot {}: {}".format(tempFile['params']['lot'], tempFile['name']).title()
+        if tempFile["params"]["tag"] in row["Name"]:
+            tempFile["params"]["lot"] = int(row["Lot #"])
+            tempFile["name"] = row["Name"]
+            tempFile["title"] = "Lot {}: {}".format(
+                tempFile["params"]["lot"], tempFile["name"]
+            ).title()
             break
     resources.append(tempFile)
 
-output = {'resources': sorted(resources, key=lambda x: x['params']['lot'])}
+output = {"resources": sorted(resources, key=lambda x: x["params"]["lot"])}
 
 print(yaml.dump(output))
